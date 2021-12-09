6f4880e61e12fd9cfe24c432dfcf00d732cb034e
ashraafa <ashraaf.alexander@gmail.com>
2021-12-09 11:34:05 +0000

    Create worksheet constants and define variables to declare new lists that are not nested

--- run.py
+++ run.py
@@ -6,13 +6,44 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
 ]
-
+"""
+Constants for declaring API credentials and accessing the
+the Google workbook
+"""
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('hangman_python')
 
-animals = SHEET.worksheet('animals').get_all_values()
+"""
+Constants for accessing worksheets in the Google workbook
+"""
+ANIMALS = SHEET.worksheet('animals').get_all_values()
+CARS = SHEET.worksheet('cars').get_all_values()
+MOVIES = SHEET.worksheet('movies').get_all_values()
+SPORTS = SHEET.worksheet('sports').get_all_values()
+HARD = SHEET.worksheet('hard').get_all_values()
+LEADERBOARD = SHEET.worksheet('leaderboard').get_all_values()
+
+"""
+Variables to declare a new list from the default nested list
+retrieved by the API
+"""
+animals = [x for list in ANIMALS for x in list]
+cars = [x for list in CARS for x in list]
+movies = [x for list in MOVIES for x in list]
+sports = [x for list in SPORTS for x in list]
+hard = [x for list in HARD for x in list]
 
 print(animals)
+print("/n")
+print(cars)
+print('/n')
+print(movies)
+print('/n')
+print(sports)
+print('/n')
+print(hard)
+
+
 
