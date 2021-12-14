404babb70810e6e1e11451d4d85ac1e9f3be9c59
ashraafa <ashraaf.alexander@gmail.com>
2021-12-14 12:20:46 +0000

    Fix long lines and spaces in api and validate files to comply with PEP8 standards

--- helpers.py
+++ helpers.py
@@ -1,20 +1,23 @@
 import os
 
+
 class colors:
     RESET = '\33[0m'
     UNDERLINE = '\033[04m'
-    BOLD ='\033[01m'
+    BOLD = '\033[01m'
     RED = '\033[31m'
     BLUE = '\033[34m'
-    GREEN ='\033[32m'
-    YELLOW ='\033[93m'
-    CYAN='\033[36m'
+    GREEN = '\033[32m'
+    YELLOW = '\033[93m'
+    CYAN = '\033[36m'
+
 
 def clear_terminal():
     """
     Function to clear terminal
     """
     os.system('cls' if os.name == 'nt' else 'clear')
+
 
 def display_hangman(attempts):
     """
--- validate.py
+++ validate.py
@@ -1,5 +1,6 @@
 import helpers
 
+
 def user_validation(player_name):
     """
     Function to validate the input received from the user
@@ -7,46 +8,52 @@ def user_validation(player_name):
     try:
         if not player_name.isalpha():
             raise ValueError(
-                f"You can only enter letters. You entered {player_name}".upper()
+                f"You can only enter letters. You entered"
+                f"{player_name}".upper()
             )
 
         elif len(player_name) > 10:
             raise ValueError(
-                f"You are only allowed 10 letters. You entered: {len(player_name)}".upper()
+                f"You are only allowed 10 letters. You entered:"
+                f"{len(player_name)}".upper()
             )
 
     except ValueError as e:
-        print(f" {helpers.colors.RED}{helpers.colors.BOLD}{e}{helpers.colors.RESET}\n") 
+        print(f" {helpers.colors.RED}{helpers.colors.BOLD}{e}"
+              f"{helpers.colors.RESET}\n")
 
-def input_validation(player_guess, game_word,letters_guessed, words_guessed):
+
+def input_validation(player_guess, game_word, letters_guessed, words_guessed):
     """
     Function to validate the input received from the user
     """
     try:
-        if len(player_guess) < len(game_word) and len(player_guess) > 1 and player_guess.isalpha():
+        if len(player_guess) < len(game_word) and len(player_guess) > 1 and \
+                player_guess.isalpha():
             raise ValueError(
-                f"{player_guess} is shorter than the hidden word and not a single letter".upper()
+                f"{player_guess} is shorter than the hidden word"
+                f"and not a single letter".upper()
             )
 
-        elif len(player_guess) > len(game_word) and len(player_guess) > 1 and player_guess.isalpha():
+        elif len(player_guess) > len(game_word) and len(player_guess) > 1 and \
+                player_guess.isalpha():
             raise ValueError(
-                f"{player_guess} is longer than the hidden word and not a single letter".upper()
+                f"{player_guess} is longer than the hidden word"
+                f"and not a single letter".upper()
             )
-
         elif not player_guess.isalpha() or player_guess == 1:
             raise ValueError(
-                f"You can only enter letters. You entered {player_guess}".upper()
+                f"You can only enter letters. You entered"
+                f"{player_guess}".upper()
             )
-        
         elif player_guess in letters_guessed:
             raise ValueError(
                 f"You already guessed {player_guess}".upper()
             )
-        
         elif player_guess in words_guessed:
             raise ValueError(
                 f"You already guessed {player_guess}".upper()
             )
-
     except ValueError as e:
-        print(f"{helpers.colors.RED}{helpers.colors.BOLD}{e}{helpers.colors.RESET}\n") 
+        print(f"{helpers.colors.RED}{helpers.colors.BOLD}{e}"
+              f"{helpers.colors.RESET}\n")
